d607d22e394277904b1c98530a6f7ac51e7df00a
Sujal Dhungana <sujaldhu372@gmail.com>
2025-03-12 09:56:24 +0545

    Update script.js

--- script.js
+++ script.js
@@ -2,13 +2,10 @@ const button = document.getElementById('celebrateButton');
 const cakeContainer = document.getElementById('cakeContainer');
 
 button.addEventListener('click', () => {
-  // Trigger the confetti effect
   confetti({
     particleCount: 100,
     spread: 70,
     origin: { y: 0.6 },
   });
-
-  // Remove the 'hidden' class from the cake container to show it
   cakeContainer.classList.remove('hidden');
 });
